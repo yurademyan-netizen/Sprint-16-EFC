7d7a9efa0048c57bf72dbf24001cafa5179b87a3
Yurii Demyan <yra.demyan@gmail.com>
2026-04-01 00:01:19 +0300

    Task 2: Implement Order and OrderDetail models. Closes #2

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,6 +1,15 @@
-﻿namespace EFC.Models
+﻿using System.Collections.Generic;
+
+namespace EFC.Models
 {
     public class Customer
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public int Discount { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,6 +1,13 @@
-﻿namespace EFC.Models
+﻿using System.Collections.Generic;
+
+namespace EFC.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public double Price { get; set; }
+
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
     }
 }
