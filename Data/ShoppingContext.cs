a70e72ae9de12d00aee1f19bc03b77215a989ada
Yurii Demyan <yra.demyan@gmail.com>
2026-04-01 00:12:14 +0300

    Task 3: Setup ShoppingContext with EF Core 8. Closes #3

---
+++ Data/ShoppingContext.cs
@@ -0,0 +1,21 @@
+﻿using EFC.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EFC.Data
+{
+    public class ShoppingContext : DbContext
+    {
+        public ShoppingContext(
+            DbContextOptions<ShoppingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Supermarket> Supermarkets { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+    }
+}
